56dbe30b77d7cc118dfc1ecdafcc6cc0aca54ff4
Annabelle Maufroy <annabelle.maufroy@gmail.com>
2025-01-30 18:53:05 +0100

    add controller and routes

--- backend/app.js
+++ backend/app.js
@@ -1,21 +1,28 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const bookRoutes = require('./routes/book');
+const userRoutes = require('./routes/user');
+
+require('dotenv').config()
+
 const app = express();
 
-app.use((req,res,next)=>{
-    console.log('requête reçu!');
-    next();
-})
-app.use((req,res,next)=>{
-    res.status(201);
-    next();
-})
-app.use((req,res, next)=>{
-    res.json({message: 'Requête reçu!'});
+mongoose.connect(process.env.URLATLAS,
+    { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log('Connexion à MongoDB réussie !'))
+    .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use(express.json());
+
+app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
     next();
 });
-app.use((req,res)=>{
-    console.log('Réponse envoyé avec succès!');
-    
-})
+
+app.use('/api/books', bookRoutes);
+app.use('/api/auth', userRoutes);
 
 module.exports = app;
