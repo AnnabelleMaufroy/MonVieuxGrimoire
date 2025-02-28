7f5d169c1fe09ad9cc933a5af13270e5e573083f
Annabelle Maufroy <annabelle.maufroy@gmail.com>
2025-02-28 18:35:23 +0100

    save

--- backend/middleware/multer-config.js
+++ backend/middleware/multer-config.js
@@ -1,13 +1,6 @@
 const multer = require('multer');
 const sharp = require('sharp');
 const path = require('path');
-const fs = require('fs');
-
-const MIME_TYPES = {
-  'image/jpg': 'jpg',
-  'image/jpeg': 'jpg',
-  'image/png': 'png'
-};
 
 const storage = multer.memoryStorage();
 const upload = multer({ storage }).single('image');
